3845e48f7dfeb34aa839a2ea1ce444f88f0f24ee
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-10-08 14:55:31 +0800

    flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <div>
+      <div className="flex flex-col justify-center h-dvh">
+        <p className="font-bold text-5xl text-center">Dont do drugs</p>
+        <p className="text-sm text-center">drugs are bad!!</p>
+      </div>
+    </div>
   );
 }
--- src/components/navbar/navigation-menu.tsx
+++ src/components/navbar/navigation-menu.tsx
@@ -4,11 +4,14 @@ import { ModeToggle } from "../mode-toggle";
 
 export function NavigationBar() {
     return (
-        <NavigationMenu className="list-none h-16">
+        <NavigationMenu className="flex flex-row justify-between min-w-full list-none h-16 sticky top-0 p-5">
+            <div className="flex flex-row">
             <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                         Home
                     </NavigationMenuLink>
+                </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
                 <Link href="/quiz" legacyBehavior passHref>
@@ -24,7 +27,10 @@ export function NavigationBar() {
                     </NavigationMenuLink>
                 </Link>
             </NavigationMenuItem>
-            <ModeToggle/>
+            </div>
+            <div>
+                <ModeToggle/>
+            </div>
         </NavigationMenu>
     )
 }
