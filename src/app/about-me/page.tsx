3b3463a60aa6646a97b64aad7de9aad3153a2339
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-10-08 11:13:19 +0800

    simple card

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,5 +1,27 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function AboutMe() {
   return (
-    <div>Hello world this is the about me route</div>
+    <div>
+        Hello world this is the about me route
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    Hello I am nicholas
+                </CardTitle>
+                <CardDescription>
+                    I like to code lots
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="flex flex-row gap-2">
+                    <p className="font-bold">Name:</p>
+                    Nicholas
+                </div>
+                <div className="flex flex-row gap-2"><p className="font-bold">Major:</p>Information Systems</div>
+                <div className="flex flex-row gap-2"><p className="font-bold">Hobbies:</p>I like to code</div>
+            </CardContent>
+        </Card>
+    </div>
   );
 }
