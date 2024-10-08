260ac10719c4781012a5c5a0386d0bde6d4acacb
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-10-08 15:30:12 +0800

    background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <div className="flex flex-col justify-center h-dvh">
+    <div className="bg-[url('/naruto2.jpg')]">
+      <div className="flex flex-col justify-center h-dvh ">
         <p className="font-bold text-5xl text-center">Dont do drugs</p>
         <p className="text-sm text-center">drugs are bad!!</p>
       </div>
