22dcbd01a849f292551eae13f1fca72c110e8c40
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-10-08 16:48:41 +0800

    hnin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 export default function Home() {
   return (
-    <div className="bg-[url('/naruto2.jpg')]">
+    <div className="">
       <div className="flex flex-col justify-center h-dvh ">
         <p className="font-bold text-5xl text-center">Dont do drugs</p>
         <p className="text-sm text-center">drugs are bad!!</p>
       </div>
+      <div className="text-xl text-center h-dvh">Do OF instead</div>
     </div>
   );
 }
--- src/components/navbar/navigation-menu.tsx
+++ src/components/navbar/navigation-menu.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "../ui/navigation-menu";
 import { ModeToggle } from "../mode-toggle";
+import Image from "next/image";
+import Logo from "@/app/assets/logo.png"
 
 export function NavigationBar() {
     return (
         <NavigationMenu className="flex flex-row justify-between min-w-full list-none h-16 sticky top-0 p-5">
             <div className="flex flex-row">
+            <Image src={Logo} height="40" width="40" alt="logo"/>
             <NavigationMenuItem>
                 <Link href="/" legacyBehavior passHref>
                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
